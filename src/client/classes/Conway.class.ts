d966c0747769b74e9a7339ede6f2b5283546e685
Peter Wildberger <pete.wildberger@gmail.com>
2018-09-11 22:04:57 -0500

    rendering correctly

--- src/client/classes/Conway.class.ts
+++ src/client/classes/Conway.class.ts
@@ -1,105 +1,107 @@
 function bool_by_percent(per: number): boolean {
-	return per > Math.floor(Math.random() * 101);
+  return per > Math.floor(Math.random() * 101);
 }
 
 export class Conway {
-	private game_width: number;
-	private game_height: number;
-	private canvas: any;
-	private ctx: CanvasRenderingContext2D;
-	private state: any[];
-	public canvasx_in_pixels: number;
-	public canvasy_in_pixels: number;
-	constructor(x: number, y: number, percentage: number, canvas: any, width: number, height: number) {
-		this.game_width = x;
-		this.game_height = y;
-		this.state = this.start_state(this.game_width, this.game_height, percentage);
-		this.canvas = canvas;
-		this.canvas.width = width;
-		this.canvas.height = height;
-		this.ctx = this.canvas.getContext('2d');
-		this.setBlockSize(width, height);
-		this.render(this.state);
-	}
-	public turn = () => {
-		console.log('turn');
-		let newState: any[] = this.generation(this.state);
-		this.render(newState);
-		this.state = this.state.splice(0, this.state.length);
-		this.state.concat(newState);
-	};
-	public render = (gen: any[]): void => {
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		let width: number = Math.floor(this.canvasx_in_pixels / this.game_width);
-		let height: number = Math.floor(this.canvasy_in_pixels / this.game_height);
-		gen.forEach((y: any[], i: number) => {
-			y.forEach((x: boolean, j: number) => {
-				if (x === true) {
-					this.ctx.beginPath();
-					this.ctx.fillStyle = 'blue';
-					this.ctx.rect(width * j, height * i, width, height);
-					this.ctx.fill();
-					this.ctx.closePath();
-				}
-			});
-		});
-	};
-	public generation = (prev: any[]): any[] => {
-		let newArr: any[] = [];
-		prev.forEach((col: any[], y) => {
-			let line: any[] = [];
-			col.forEach((cell: boolean, x) => {
-				line.push(this.rule(x, y, cell, prev));
-			});
-			newArr.push(line);
-		});
-		return newArr;
-	};
-	private rule = (x: number, y: number, alive: boolean, prev: any[]): boolean => {
-		const living = this.living_neighbors(x, y, prev);
-		if (alive) {
-			if (living < 2 || living > 3) {
-				//Any live cell with fewer than two live neighbors dies, as if by under population.
-				//Any live cell with more than three live neighbors dies, as if by overpopulation.
-				return false;
-			} else if (living === 2 || living === 3) {
-				//Any live cell with two or three live neighbors lives on to the next generation.
-				return true;
-			}
-		} else if (alive === false && living === 3) {
-			//Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-			return true;
-		}
-	};
-	private living_neighbors = (posx: number, posy: number, prev: any[]): number => {
-		// returns the amount of 'living' neighbors
-		let living: number = 0;
-		for (let y = posy - 1; y <= posy + 1; y++) {
-			for (let x = posx - 1; x <= posx + 1; x++) {
-				if (x === posx && y === posy) {
-					// skip itself
-				} else if (y > -1 && x > -1 && x < this.game_width && y < this.game_height) {
-					if (prev[y][x] === true) {
-						console.log(prev[y][x]);
-						living++;
-					}
-				}
-			}
-		}
-		return living;
-	};
-	private start_state(x: number, y: number, percentage: number): any[] {
-		let state: any[] = [];
-		for (let i = 0; i < y; i++) {
-			state.push([]);
-			for (let j = 0; j < x; j++) {
-				state[i].push(bool_by_percent(percentage));
-			}
-		}
-		return state;
-	}
-	public setBlockSize(width: number, height: number) {
-		this.canvasx_in_pixels = width;
-		this.canvasy_in_pixels = height;
-	}
+  private game_width: number;
+  private game_height: number;
+  private canvas: any;
+  private ctx: CanvasRenderingContext2D;
+  private state: any[];
+  public canvasx_in_pixels: number;
+  public canvasy_in_pixels: number;
+  constructor(x: number, y: number, percentage: number, canvas: any, width: number, height: number) {
+    this.game_width = x;
+    this.game_height = y;
+    this.state = this.start_state(this.game_width, this.game_height, percentage);
+    this.canvas = canvas;
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx = this.canvas.getContext('2d');
+    this.setBlockSize(width, height);
+  }
+  public turn = () => {
+    if (this.state.length === 0) {
+      return;
+    }
+    let newState: any[] = this.generation(this.state);
+    this.state.splice(0, this.state.length);
+    this.state = this.state.concat(newState);
+    this.render(this.state);
+  };
+  public render = (gen: any[]): void => {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    let width: number = Math.floor(this.canvasx_in_pixels / this.game_width);
+    let height: number = Math.floor(this.canvasy_in_pixels / this.game_height);
+    gen.forEach((y: any[], i: number) => {
+      y.forEach((x: boolean, j: number) => {
+        if (x === true) {
+          this.ctx.beginPath();
+          this.ctx.fillStyle = `blue`;
+          this.ctx.rect(width * j, height * i, width, height);
+          this.ctx.fill();
+          this.ctx.closePath();
+        }
+      });
+    });
+  };
+  public generation = (prev: any[]): any[] => {
+    let newArr: any[] = [];
+    prev.forEach((col: any[], y) => {
+      let line: any[] = [];
+      col.forEach((cell: boolean, x) => {
+        line.push(this.rule(x, y, cell, prev));
+      });
+      newArr.push(line);
+    });
+    return newArr;
+  };
+  private rule = (x: number, y: number, alive: boolean, prev: any[]): boolean => {
+    const living = this.living_neighbors(x, y, prev);
+    if (alive) {
+      if (living < 2 || living > 3) {
+        //Any live cell with fewer than two live neighbors dies, as if by under population.
+        //Any live cell with more than three live neighbors dies, as if by overpopulation.
+        return false;
+      } else if (living === 2 || living === 3) {
+        //Any live cell with two or three live neighbors lives on to the next generation.
+        return true;
+      }
+    } else if (alive === false && living === 3) {
+      //Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+      return true;
+    } else {
+      return false;
+    }
+  };
+  private living_neighbors = (posx: number, posy: number, prev: any[]): number => {
+    // returns the amount of 'living' neighbors
+    let living: number = 0;
+    for (let y = posy - 1; y <= posy + 1; y++) {
+      for (let x = posx - 1; x <= posx + 1; x++) {
+        if (x === posx && y === posy) {
+          // skip itself
+        } else if (y > -1 && x > -1 && x < this.game_width && y < this.game_height) {
+          if (prev[y][x] === true) {
+            living++;
+          }
+        }
+      }
+    }
+    return living;
+  };
+  private start_state(x: number, y: number, percentage: number): any[] {
+    let state: any[] = [];
+    for (let i = 0; i < y; i++) {
+      state.push([]);
+      for (let j = 0; j < x; j++) {
+        state[i].push(bool_by_percent(percentage));
+      }
+    }
+    return state;
+  }
+  public setBlockSize(width: number, height: number) {
+    this.canvasx_in_pixels = width;
+    this.canvasy_in_pixels = height;
+  }
 }
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,24 +1,25 @@
 import { Conway } from './classes/Conway.class';
 
 function resize() {
-	let canvas: any = document.getElementById('gOL');
-	canvas.outerHeight = window.outerHeight - canvas.offsetTop - Math.abs(canvas.outerHeight - canvas.innerHeight);
+  let canvas: any = document.getElementById('gOL');
+  canvas.outerHeight = window.outerHeight - canvas.offsetTop - Math.abs(canvas.outerHeight - canvas.innerHeight);
 }
 
 (function main() {
-	console.log('Conway App running');
-	let width: number = document.getElementById('gOL').clientWidth;
-	let height: number = document.getElementById('gOL').clientHeight;
-	window.onload = () => {
-		window.addEventListener(
-			'resize',
-			() => {
-				resize();
-				conway.setBlockSize(document.getElementById('gOL').clientWidth, document.getElementById('gOL').clientHeight);
-			},
-			false
-		);
-	};
-	const conway = new Conway(100, 100, 10, document.getElementById('gOL'), width, height);
-	setInterval(conway.turn, 2000);
+  console.log('Conway App running');
+  let width: number = document.getElementById('gOL').clientWidth;
+  let height: number = document.getElementById('gOL').clientHeight;
+  window.onload = () => {
+    window.addEventListener(
+      'resize',
+      () => {
+        resize();
+        conway.setBlockSize(document.getElementById('gOL').clientWidth, document.getElementById('gOL').clientHeight);
+      },
+      false
+    );
+  };
+  const conway = new Conway(200, 120, 20, document.getElementById('gOL'), width, height);
+
+  setInterval(conway.turn, 200);
 })();
